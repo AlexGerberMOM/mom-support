9ba26fc4dbd07044cedd678fcb8ce7f190df9b60
AlexGerberMOM <87760324+AlexGerberMOM@users.noreply.github.com>
2021-07-21 16:38:39 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -18,31 +18,31 @@ def start(update, context):
 
 def forward_to_chat(update, context):
     """{ 
-        'message_id': 5, 
-        'date': 1605106546, 
-        'chat': {'id': 427003405, 'type': 'private', 'username': 'andrfrost', 'first_name': 'Alex', 'last_name': 'Bontillgut'}, 
-        'text': 'TEST QOO', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
-        'from': {'id': 427003405, 'first_name': 'Alex', 'is_bot': False, 'last_name': 'Bontillgut', 'username': 'andrfrost', 'language_code': 'en'}
+        'message_id': 716862, 
+        'date': 1626874206, 
+        'chat': {'id': 1309138849, 'type': 'private', 'username': 'mummicoffee', 'first_name': 'Mummi', 'last_name': 'Coffee'}, 
+        'text': 'G', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
+        'from': {'id': 1309138849, 'first_name': 'Mummi', 'is_bot': False, 'last_name': 'Coffee', 'username': 'mummicoffee', 'language_code': 'ru'}
     }"""
     update.message.forward(chat_id=TELEGRAM_SUPPORT_CHAT_ID)
 
 
 def forward_to_user(update, context):
     """{
-        'message_id': 10, 'date': 1605106662, 
-        'chat': {'id': -484179205, 'type': 'group', 'title': '☎️ SUPPORT CHAT', 'all_members_are_administrators': True}, 
+        'message_id': 716856, 'date': 1626874043, 
+        'chat': {'id': -460338920, 'type': 'group', 'title': 'support', 'all_members_are_administrators': True}, 
         'reply_to_message': {
             'message_id': 9, 'date': 1605106659, 
-            'chat': {'id': -484179205, 'type': 'group', 'title': '☎️ SUPPORT CHAT', 'all_members_are_administrators': True}, 
-            'forward_from': {'id': 49820636, 'first_name': 'Alex', 'is_bot': False, 'last_name': 'Bontillgut', 'andrfrost': 'Bontillgut', 'language_code': 'en'}, 
-            'forward_date': 1605106658, 
+            'chat': {'id': -460338920, 'type': 'group', 'title': 'support', 'all_members_are_administrators': True}, 
+            'forward_from': {'id': 1309138849, 'first_name': 'Mummi', 'is_bot': False, 'last_name': 'Coffee', 'mummicoffee': 'Mummi', 'language_code': 'ru'}, 
+            'forward_date': 1605106659, 
             'text': 'g', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 
             'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
-            'from': {'id': 1309138849, 'first_name': 'SUPPORT', 'is_bot': True, 'username': 'lolkek'}
+            'from': {'id': 1949519631, 'first_name': 'Mummi Coffee', 'is_bot': True, 'username': 'mom_coffee_bot'}
         }, 
         'text': 'ggg', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 
         'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
-        'from': {'id': 427003405, 'first_name': 'Alex', 'is_bot': False, 'last_name': 'Bontillgut', 'username': 'andrfrost', 'language_code': 'en'}
+        'from': {'id': 1309138849, 'first_name': 'Mummi', 'is_bot': False, 'last_name': 'Coffee', 'username': 'mummicoffee', 'language_code': 'ru'}
     }"""
     user_id = update.message.reply_to_message.forward_from.id
     context.bot.copy_message(
