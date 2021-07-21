75281d89d59b197193de072c263b0d8306eb3c16
AlexGerberMOM <87760324+AlexGerberMOM@users.noreply.github.com>
2021-07-21 14:57:03 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -20,9 +20,9 @@ def forward_to_chat(update, context):
     """{ 
         'message_id': 5, 
         'date': 1605106546, 
-        'chat': {'id': 49820636, 'type': 'private', 'username': 'danokhlopkov', 'first_name': 'Daniil', 'last_name': 'Okhlopkov'}, 
+        'chat': {'id': 427003405, 'type': 'private', 'username': 'andrfrost', 'first_name': 'Alex', 'last_name': 'Bontillgut'}, 
         'text': 'TEST QOO', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
-        'from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': 'danokhlopkov', 'language_code': 'en'}
+        'from': {'id': 427003405, 'first_name': 'Alex', 'is_bot': False, 'last_name': 'Bontillgut', 'username': 'andrfrost', 'language_code': 'en'}
     }"""
     update.message.forward(chat_id=TELEGRAM_SUPPORT_CHAT_ID)
 
@@ -34,15 +34,15 @@ def forward_to_user(update, context):
         'reply_to_message': {
             'message_id': 9, 'date': 1605106659, 
             'chat': {'id': -484179205, 'type': 'group', 'title': '☎️ SUPPORT CHAT', 'all_members_are_administrators': True}, 
-            'forward_from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'danokhlopkov': 'okhlopkov', 'language_code': 'en'}, 
+            'forward_from': {'id': 49820636, 'first_name': 'Alex', 'is_bot': False, 'last_name': 'Bontillgut', 'andrfrost': 'Bontillgut', 'language_code': 'en'}, 
             'forward_date': 1605106658, 
             'text': 'g', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 
             'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
-            'from': {'id': 1440913096, 'first_name': 'SUPPORT', 'is_bot': True, 'username': 'lolkek'}
+            'from': {'id': 1309138849, 'first_name': 'SUPPORT', 'is_bot': True, 'username': 'lolkek'}
         }, 
         'text': 'ggg', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 
         'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
-        'from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': 'danokhlopkov', 'language_code': 'en'}
+        'from': {'id': 427003405, 'first_name': 'Alex', 'is_bot': False, 'last_name': 'Bontillgut', 'username': 'andrfrost', 'language_code': 'en'}
     }"""
     user_id = update.message.reply_to_message.forward_from.id
     context.bot.copy_message(
